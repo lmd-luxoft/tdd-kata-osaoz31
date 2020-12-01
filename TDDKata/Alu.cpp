f26ed4698a1567b1b32fca1563aec4bcc09ddc3a
Овчинников Сергей Александрович <s.ovchinnikov@owen.ru>
2020-12-01 16:33:15 +0300

    Изменил Alu::Add() для задачи 3

--- TDDKata/Alu.cpp
+++ TDDKata/Alu.cpp
@@ -6,7 +6,7 @@ int Alu::Add(char * numbers)
 {
 	int result = 0;
 	int q = strlen(numbers);//длина принятой строки
-	const int max_q_r = 10; //максимальное количество разрядов
+	const int max_q_r = 9; //максимальное количество разрядов
 	char str_num[max_q_r] = {0};
 
 	if (q) {
@@ -16,13 +16,12 @@ int Alu::Add(char * numbers)
 		while (i < q) {
 			s[0] = numbers[i];
 
-			if (!strcmp(",", s)) {
+			if (!(strcmp(",", s) && (strcmp("\n", s)))) {
 				result = result + atoi(str_num);
 				memset(str_num, '\0', max_q_r);
-			} else {
-				s[0] = numbers[i];
+			} else 
 				strncat(str_num, s, 1);
-			}
+			
 			i++;
 		}
 
